9b7888e10b36db8f0ed2d407632f17c81580cdd3
M Haidar Hanif <git@mhaidarhanif.com>
2022-04-23 17:20:33 +0700

    feat: cta in home hero

--- app/contents/home/hero.tsx
+++ app/contents/home/hero.tsx
@@ -1,4 +1,4 @@
-import { useTheme } from '~/components';
+import { RemixLink, useTheme } from '~/components';
 
 import type { FunctionComponent } from 'react';
 
@@ -23,11 +23,31 @@ export const HomeHero: FunctionComponent<HomeHeroProps> = () => {
         }")`,
       }}
     >
-      <div className="comic-box my-32">
-        <h1 className="font-comic max-w-[35ch] text-center text-3xl font-extrabold leading-normal">
-          Pioneering RetailFi with user friendly and curated Crypto
-          Decentralized Exchange (DEX)
-        </h1>
+      <div>
+        <div className="comic-box my-32 flex flex-col space-y-4">
+          <h1 className="font-comic max-w-[35ch] text-center text-3xl font-extrabold leading-normal">
+            Pioneering RetailFi with user friendly and curated Crypto
+            Decentralized Exchange (DEX)
+          </h1>
+          <section className="flex-section justify-center space-x-4">
+            <RemixLink
+              to="/blog"
+              className="btn btn-lg btn-solid rounded-base"
+              data-color="primary"
+            >
+              Read Updates
+            </RemixLink>
+            <RemixLink
+              to="/whitepaper"
+              className="btn-outline btn btn-lg rounded-base"
+              data-color="primary"
+            >
+              Read Whitepaper
+            </RemixLink>
+          </section>
+        </div>
+
+        <div>Character</div>
       </div>
     </div>
   );
