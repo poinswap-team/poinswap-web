7089cbd4854625312b75a5b986fe966f196e56af
M Haidar Hanif <git@mhaidarhanif.com>
2022-04-30 02:55:29 +0700

    feat: subscribe form

--- app/routes/privatesale.tsx
+++ app/routes/privatesale.tsx
@@ -4,7 +4,10 @@ export default function PrivateSaleRoute() {
   return (
     <Layout className="prose dark:prose-invert">
       <h1 className="font-comic text-primary-500">Private Sale</h1>
-      <p>Private Sale is coming soon!</p>
+      <p>Subsribe form</p>
+      <form action="">
+        <input type="email" />
+      </form>
     </Layout>
   );
 }
