495402cebe76a174ec1b1d126ea40c73319760c5
M Haidar Hanif <git@mhaidarhanif.com>
2022-04-30 08:29:29 +0700

    feat: subscribe via convertkit

--- app/routes/privatesale.tsx
+++ app/routes/privatesale.tsx
@@ -1,13 +1,29 @@
-import { Layout } from '~/components';
+import { useActionData, useTransition } from 'remix';
+
+import { Anchor, Layout } from '~/components';
+import { PrivateSaleForm } from '~/contents';
+import { actionSubscribe } from '~/features';
+
+import type { ActionFunction } from 'remix';
+
+export const action: ActionFunction = actionSubscribe;
 
 export default function PrivateSaleRoute() {
+  const transition = useTransition();
+  const actionData = useActionData();
+
   return (
     <Layout className="prose dark:prose-invert">
       <h1 className="font-comic text-primary-500">Private Sale</h1>
-      <p>Subsribe form</p>
-      <form action="">
-        <input type="email" />
-      </form>
+      <p>
+        The token private sale is opening soon. For now, please subscribe here
+        for updates. As you will be invited through email. You can also{' '}
+        <Anchor href="https://poinswap.com/discord">join our Discord</Anchor> to
+        get the updates, but we'll prioritize the ones who subscribe their email
+        first.
+      </p>
+
+      <PrivateSaleForm transition={transition} actionData={actionData} />
     </Layout>
   );
 }
